db480664c08190a28c27884df9e1739ec4305d58
seongwoo-choi <how0326@naver.com>
2021-10-19 16:59:33 +0900

    Repository Interface 메소드

--- src/main/java/com/example/bookmanager/domain/User.java
+++ src/main/java/com/example/bookmanager/domain/User.java
@@ -2,16 +2,30 @@ package com.example.bookmanager.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@RequiredArgsConstructor
 @Builder
+@Entity
 public class User {
 
+    // User 라는 테이블의 pk, 1 씩 자동으로 증가
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private String email;
+
     private LocalDateTime created_at;
     private LocalDateTime updated_at;
 
---
+++ src/test/java/resources/data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (1, 'ab', 'ab@ab.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (2, 'bb', 'bb@ab.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (3, 'cb', 'cb@ab.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (4, 'db', 'db@ab.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (5, 'ab', 'eb@ab.com', now(), now());
