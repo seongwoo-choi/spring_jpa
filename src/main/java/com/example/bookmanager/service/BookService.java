723b2c5ae377400babbb5bb25b83eac27514c6bf
seongwoo-choi <how0326@naver.com>
2021-10-28 14:32:34 +0900

    @Transactional 에서 CheckedException 이 발생하는 경우
    - 트랜잭션 내에서 CheckedException(Exception) 가 발생하면 롤백되지 않고 데이터베이스에 데이터가 반영된다. UnCheckedException(RuntimeException) 과 다른 점!
    - 즉, CheckedException 의 경우 모든 문제를 개발자가 책임지게 된다. 그래서 CheckedException 의 경우 에러를 try, catch 로 체크를 하고 핸들링하게 되어있다.

--- src/main/java/com/example/bookmanager/service/BookService.java
+++ src/main/java/com/example/bookmanager/service/BookService.java
@@ -18,7 +18,7 @@ public class BookService {
     private final AuthorRepository authorRepository;
 
     @Transactional
-    public void putBookAndAuthor() {
+    public void putBookAndAuthor() throws Exception {
         Book book = new Book();
         book.setName("JPA 시작");
 
@@ -28,6 +28,8 @@ public class BookService {
         author.setName("martin");
 
         authorRepository.save(author);
+
+        throw new Exception("오류가 나서 DB commit 이 발생하지 않습니다.");
     }
 
 }
