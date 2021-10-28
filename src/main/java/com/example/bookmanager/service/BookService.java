9435d7b099095e6641b3eae0c32000262537c29e
seongwoo-choi <how0326@naver.com>
2021-10-28 17:42:36 +0900

    Isolation REPEATABLE_READ 와 SERIALIZABLE
    - REPEATABLE_READ 에선 자기 트랜잭션이 시작될 때 조회했던 데이터를 저장하고 있다가 트랜잭션이 끝나기 전까지 스냅샷을 계속해서 리턴해준다. 그래서 내부에서 반복적으로 데이터를 조회하더라도 변경된 값이 나오지 않고 맨 처음 스냅샷을 찍은 값이 나타난다.
    - 트랜잭션이 완전히 종료되고 나서야 변경된 값이 노출된다, Phantom Read 가 발생한다! ⇒ 데이터가 안보이는 데 처리가 되는 상태
    
    - SERIALIZABLE 커밋이 일어나지 않은 트랜잭션이 존재하면 락을 통해서 waiting, 커밋이 실행되어야 추가적인 로직이 실행 ⇒ Phantom Read 방지
    - 데이터를 변경하려고 하면 다른 쪽 트랜잭션이 끝날 때 까지 무조건 기다린 후에 처리한다. 데이터의 정합성은 100% 가 되지만 그만큼 웨이팅이 길어져서 성능은 떨어진다.

--- src/main/java/com/example/bookmanager/service/BookService.java
+++ src/main/java/com/example/bookmanager/service/BookService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 @Service
 @RequiredArgsConstructor
 public class BookService {
@@ -17,6 +19,7 @@ public class BookService {
     // 의존성 주입
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final EntityManager entityManager;
 
 
     // rollbackFor = Exception.class 을 사용하면 Exception 이 CheckedException 임에도 불구하고 rollback 이 되게 된다.
@@ -36,17 +39,25 @@ public class BookService {
     }
 
     // READ_UNCOMMITTED => 커밋되지 않는(트랜잭션 처리중인) 데이터에 대한 읽기를 허용, dirty read 문제와 데이터 정합성 문제
-    @Transactional(isolation = Isolation.READ_COMMITTED)
+    // SERIALIZABLE 커밋이 일어나지 않은 트랜잭션이 존재하면 락을 통해서 waiting, 커밋이 실행되어야 추가적인 로직이 실행
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public void get(Long id) {
         System.out.println(">>>> " + bookRepository.findById(id));
         System.out.println(">>>> " + bookRepository.findAll());
 
+        entityManager.clear();
+
+        // 두 번 째 findById 이기 때문에 영속성 컨텍스트에 캐시되어있다. => entityManger 에서 캐싱을 정리해줘야 한다.
         System.out.println(">>>> " + bookRepository.findById(id));
         System.out.println(">>>> " + bookRepository.findAll());
 
-        Book book = bookRepository.findById(1L).get();
-        book.setName("바뀔까?");
-        bookRepository.save(book);
+        bookRepository.update();
+
+        entityManager.clear();
+
+//        Book book = bookRepository.findById(1L).get();
+//        book.setName("바뀔까?");
+//        bookRepository.save(book);
 
     }
 
